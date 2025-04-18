572e2b2348465b089efb43a9510f0b94aea0c83e
ZmeiMatvei <markovshchenkov.m@rusrobots.ru>
2025-04-18 16:58:01 +0300

    .idea

---
+++ ifelse.py
@@ -0,0 +1,97 @@
+# if условие:
+# блок кода
+# else
+# блок кода
+#i = int(input())
+#if i // 2 != 0:
+#    print(i, 'нечётное')
+#else:
+#    print(i, 'чётное')
+
+#pas1 = str(input())
+#pas2 = str(input())
+#if pas1 == pas2:
+#    print("Пароль принят")
+#else:
+#    print("Пароль не принят")
+
+#num = int(input())
+#if num % 2 == 0:
+#    print("Четное")
+#else:
+#    print("Нечетное")
+
+#num = int(input())
+#a = num // 1000
+#b = (num // 100) % 10
+#c = (num // 10) % 10
+#d = num % 10
+#if a+d == b-c:
+#    print("ДА")
+#else:
+#    print("НЕТ")
+
+#age = int(input())
+#if 0<=age<18:
+#    print("Доступ запрещен")
+#else:
+#    print("Доступ разрешен")
+
+#a = int(input())
+#b = int(input())
+#c = int(input())
+#if b - a == c - b:
+#    print("YES")
+#else:
+#    print("NO")
+
+#numbers_1 = int(input())
+#numbers_2 = int(input())
+#if numbers_1 < numbers_2:
+#    print(numbers_1)
+#else:
+#    print(numbers_2)
+
+#num_1 = int(input())
+#num_2 = int(input())
+#num_3 = int(input())
+#num_4 = int(input())
+#num_m1 = 0
+#num_m2 = 0
+#if num_1 <= num_2:
+#    num_m1 = num_1
+#else:
+#    num_m1 = num_2
+#if num_3 <= num_4:
+#    num_m2 = num_3
+#else:
+#    num_m2 = num_4
+#if num_m1 <= num_m2:
+#    print(num_m1)
+#else:
+#    print(num_m2)
+
+#age = int(input())
+#if age <= 13:
+#    print("детство")
+#if 14 <= age <= 24:
+#    print("молодость")
+#if 25 <= age <= 59:
+#    print("зрелость")
+#if 60 <= age:
+#    print("старость")
+
+#num_1 = int(input())
+#num_2 = int(input())
+#num_3 = int(input())
+#a = 0
+#if num_1 > 0:
+#    a = num_1
+#if num_2 > 0:
+#    a = a + num_2
+#if num_3 > 0:
+#    a = a + num_3
+#if a <= 0:
+#    a = 0
+#print(a)
+
